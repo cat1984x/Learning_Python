5299aa701b5d7ca860e50729d1c88c64533de947
Chen Yushen <chenyushen1989@163.com>
2017-10-28 16:29:53 +0800

    dic
    dic

---
+++ exercises/6/6_2_dic_favor_num.py
@@ -0,0 +1,10 @@
+chen={
+	'chen':3,
+	'zhou':7,
+	'GG':23,
+	'v':12,
+	'MM':8,
+	}
+
+for name, num in chen.items():
+	print(name+':'+str(num))
---
+++ exercises/6/6_3_dic_python_word.py
@@ -0,0 +1,10 @@
+keyword={
+	'print':'print out',
+	'for':'create a loop',
+	'if':'decide is or mot',
+	'range':'a list num',
+	'else':'otherwise',
+	}
+
+for word, meaning in keyword.items():
+	print(word+':'+meaning)
