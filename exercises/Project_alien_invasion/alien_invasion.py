8294d75414e69292157cf47e871a528d8455a6a9
Chen Yushen <chenyushen1989@163.com>
2017-10-31 21:32:37 +0800

    13
    13

--- exercises/Project_alien_invasion/alien_invasion.py
+++ exercises/Project_alien_invasion/alien_invasion.py
@@ -3,31 +3,36 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf 
 from pygame.sprite import Group
 
 
 def run_game():
-	#初始化游戏
-	pygame.init()
-	ai_setting = Settings()
-	screen = pygame.display.set_mode(
-		(ai_setting.screen_width,ai_setting.screen_height))		
-	pygame.display.set_caption("Alien Invasion")
-	
-	#创建飞船
-	ship = Ship(ai_setting,screen)	
-	
-	#创建存储子弹的编组
-	bullets = Group()
-	
-	#开始游戏主循环
-	while True:		
-		gf.check_events(ai_setting,screen,ship,bullets)
-		ship.update()
-		gf.update_bullets(bullets)		
-		gf.update_screen(ai_setting,screen,ship,bullets)		
+    #初始化游戏
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))     
+    pygame.display.set_caption("Alien Invasion")
+    
+    #创建飞船
+    ship = Ship(ai_settings,screen)  
+    
+    #创建外星人
+    alien = Alien(screen,ai_settings)
+    
+    
+    #创建存储子弹的编组
+    bullets = Group()
+    
+    #开始游戏主循环
+    while True:     
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)      
+        gf.update_screen(ai_settings,screen,ship,bullets,alien)        
 
-		
-		
+        
+        
 run_game()
