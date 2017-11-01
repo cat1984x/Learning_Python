b660ee4131b99d73dc6cd410fd050db61676672a
Chen Yushen <chenyushen1989@163.com>
2017-11-01 13:07:08 +0800

    13
    13

--- exercises/Project_alien_invasion/settings.py
+++ exercises/Project_alien_invasion/settings.py
@@ -1,30 +1,4 @@
 class Settings():
-<<<<<<< HEAD
-<<<<<<< HEAD
-    def __init__(self):     
-        '''存储游戏所有设置的类'''
-        
-        #设置背景色
-        self.screen_width = 1200
-        self.screen_height = 800        
-        self.bg_color = (230,230,230)   
-        
-        #飞船的设置
-        self.ship_speed_factor = 1.5
-        
-        #外星人设置
-        self.alien_speed_factor = 1
-        
-      
-        #子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60,60,60
-        self.bullet_allowed = 3
-=======
-=======
->>>>>>> parent of 98eac30... 13
 	def __init__(self):		
 		'''存储游戏所有设置的类'''
 		
@@ -42,7 +16,3 @@ class Settings():
 		self.bullet_height = 15
 		self.bullet_color = 60,60,60
 		self.bullet_allowed = 3
-<<<<<<< HEAD
->>>>>>> parent of 98eac30... 13
-=======
->>>>>>> parent of 98eac30... 13
